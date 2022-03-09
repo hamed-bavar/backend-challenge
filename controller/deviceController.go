5af52d8b926d5fab2b293e03e64d251a176277c8
hamed <hamedbavarnegin@gmail.com>
2022-03-09 21:04:48 +0330

    Add validation  package

--- controller/deviceController.go
+++ controller/deviceController.go
@@ -5,6 +5,7 @@ import (
 	service "challenge/serivce"
 	"challenge/utils"
 	"encoding/json"
+	"github.com/go-playground/validator/v10"
 	"net/http"
 )
 
@@ -16,7 +17,13 @@ func (dc *DeviceController) CreateDevice(w http.ResponseWriter, r *http.Request)
 	var device domain.Device
 	//validate request
 	if err := json.NewDecoder(r.Body).Decode(&device); err != nil {
-		utils.WriteResponse(w, http.StatusBadRequest, err.Error())
+		utils.WriteResponse(w, http.StatusBadRequest, "invalid fields")
+		return
+	}
+	validate := validator.New()
+	validationError := validate.Struct(device)
+	if validationError != nil {
+		utils.WriteResponse(w, http.StatusBadRequest, validationError.Error())
 		return
 	}
 	response, appError := dc.Service.CreateDevice(&device)
