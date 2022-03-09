8f35b9af0186d2e5b8d166d2fb6cca3fa8423e1a
hamed <hamedbavarnegin@gmail.com>
2022-03-09 19:05:34 +0330

    Change db connect function

--- utils/dbConnect.go
+++ utils/dbConnect.go
@@ -4,14 +4,17 @@ import (
 	"challenge/lib/errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
 )
 
 func GetDbClient() (*dynamodb.DynamoDB, *errors.AppError) {
-	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-west-2")})
+	credential := credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), "")
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-west-2"), Credentials: credential})
 	if err != nil {
-		fmt.Println("Error while creating session:", err)
+		fmt.Println("Error while creating session:", sess)
 		return nil, errors.InternalServerError("Internal Server Error from dynamo db")
 	}
 	fmt.Println("Session created successfully", err)
