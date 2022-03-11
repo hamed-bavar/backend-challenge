ba7c18dfe2c2a9688f66772f2cd8ad9fb1f28e4f
hamed <hamedbavarnegin@gmail.com>
2022-03-12 02:06:04 +0330

    Add test for get  device vy id  repository

--- repository/deviceRepository_test.go
+++ repository/deviceRepository_test.go
@@ -11,24 +11,8 @@ import (
 	"testing"
 )
 
-type MyError struct{}
-
-func (m *MyError) Error() string {
-	return "boom"
-}
-func Test_Should_Return_Created_Device(t *testing.T) {
-	mockDevice := domain.Device{
-		Id:          "1234",
-		DeviceModel: "mercedes",
-		Name:        "your car",
-		Note:        "nice car",
-		Serial:      "8765432",
-	}
-	marshaledDevice, _ := dynamodbattribute.MarshalMap(mockDevice)
-	dynamoDbItem := &dynamodb.PutItemInput{
-		Item:      marshaledDevice,
-		TableName: aws.String("Device"),
-	}
+func Test_Should_Return_Create_Device(t *testing.T) {
+	dynamoDbItem, mockDevice := getMockDevice()
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockDB := mockDynamoDBAPI.NewMockDynamoDBAPI(ctrl)
@@ -37,7 +21,77 @@ func Test_Should_Return_Created_Device(t *testing.T) {
 	createdDevice, _ := repo.Create(&mockDevice)
 	assert.Equal(t, mockDevice, *createdDevice)
 }
-func Test_Should_Return_Server_Error(t *testing.T) {
+func Test_Should_Return_Server_Error_While_Creating_Device(t *testing.T) {
+	dynamoDbItem, mockDevice := getMockDevice()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockDB := mockDynamoDBAPI.NewMockDynamoDBAPI(ctrl)
+	mockDB.EXPECT().PutItem(dynamoDbItem).Return(nil, &MyError{})
+	repo := NewDeviceRepositoryDb(mockDB)
+	_, repoError := repo.Create(&mockDevice)
+
+	assert.Equal(t, "Internal Server Error", (*repoError).Message)
+}
+func Test_Should_Return_Device_With_Specific_Id(t *testing.T) {
+	_, expectedDevice := getMockDevice()
+	dynamoDbItem := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String("1234"),
+			},
+		},
+		TableName: aws.String("Device"),
+	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockDB := mockDynamoDBAPI.NewMockDynamoDBAPI(ctrl)
+	mockDB.EXPECT().GetItem(dynamoDbItem).Return(&dynamodb.GetItemOutput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String("1234"),
+			},
+			"deviceModel": {
+				S: aws.String("mercedes"),
+			},
+			"name": {
+				S: aws.String("your car"),
+			},
+			"note": {
+				S: aws.String("nice car"),
+			},
+			"serial": {
+				S: aws.String("8765432"),
+			},
+		},
+	}, nil)
+	repo := NewDeviceRepositoryDb(mockDB)
+	actualDevice, _ := repo.FindById("1234")
+	assert.Equal(t, expectedDevice, *actualDevice)
+}
+func Test_Should_Return_Server_Error_While_Getting_Device(t *testing.T) {
+	dynamoDbItem := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String("1234"),
+			},
+		},
+		TableName: aws.String("Device"),
+	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockDB := mockDynamoDBAPI.NewMockDynamoDBAPI(ctrl)
+	mockDB.EXPECT().GetItem(dynamoDbItem).Return(nil, &MyError{})
+	repo := NewDeviceRepositoryDb(mockDB)
+	_, err := repo.FindById("1234")
+	assert.Equal(t, "Internal Server Error", (*err).Message)
+}
+
+type MyError struct{}
+
+func (m *MyError) Error() string {
+	return "boom"
+}
+func getMockDevice() (*dynamodb.PutItemInput, domain.Device) {
 	mockDevice := domain.Device{
 		Id:          "1234",
 		DeviceModel: "mercedes",
@@ -50,12 +104,5 @@ func Test_Should_Return_Server_Error(t *testing.T) {
 		Item:      marshaledDevice,
 		TableName: aws.String("Device"),
 	}
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	mockDB := mockDynamoDBAPI.NewMockDynamoDBAPI(ctrl)
-	mockDB.EXPECT().PutItem(dynamoDbItem).Return(nil, &MyError{})
-	repo := NewDeviceRepositoryDb(mockDB)
-	_, repoError := repo.Create(&mockDevice)
-
-	assert.Equal(t, (*repoError).Message, "Internal Server Error")
+	return dynamoDbItem, mockDevice
 }
