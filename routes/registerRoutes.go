d1ca33c110b5882ee42e18c388d4cbda6e1c84d2
hamed <hamedbavarnegin@gmail.com>
2023-08-11 11:53:06 +0330

    Test

--- routes/registerRoutes.go
+++ routes/registerRoutes.go
@@ -5,11 +5,12 @@ import (
 	"challenge/repository"
 	"challenge/service"
 	"challenge/utils"
+
 	"github.com/gorilla/mux"
 )
 
 func Register(router *mux.Router) {
-	//create db instance
+	//create db instanc
 	dbClient, _ := utils.GetDbClient()
 	deviceRepo := repository.NewDeviceRepositoryDb(dbClient)
 	deviceController := controller.DeviceController{Service: service.NewDeviceService(deviceRepo)}
